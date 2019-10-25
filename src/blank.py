b92dfde122082d22d2362e5cb0757da3a75063dc
Colin Bitterfield <colin@bitterfield.com>
2019-10-24 22:00:26 -0700

    Update blank.py

--- src/blank.py
+++ src/blank.py
@@ -7,6 +7,7 @@ Created on Oct 12, 2019
 @author: colin
 
 '''
+from fileinput import filename
 
 __author__ = "Colin Bitterfield"
 __copyright__ = "Copyright 2019, " + __author__
@@ -43,3 +44,96 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+    
+    
+    
+    
+### Build an argparsing function ###
+### Function requires variables and gets script name from the operating system
+
+def getArgs(argv=None):
+    import argparse
+    import os
+    parser = argparse.ArgumentParser()
+    parser.prog =os.path.basename(__file__)
+    parser.description = "This is where the command-line utility's description goes."
+    parser.epilog = "This is where the command-line utility's epilog goes."
+    parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
+    
+    parser.add_argument('[-SHORT_FLAG]','[--LONG_FLAG]',
+                        help = '[Help Message]',
+                        action='store', # or use store_true/false
+                        type = str, # or use int, filename
+                        required=False, # or set to true
+                        dest='xls_filename', # variable to be stored
+                        choices=['rock', 'paper', 'scissors'], # Set valid choices
+                        default='[DEFAULT]' # Set a default value 
+                        ) 
+    
+    parser.add_argument('-d','--debug',
+                        help='Turn on debugging, and set log level',
+                        action='store',
+                        type = str,
+                        required=False,
+                        dest='LOG',
+                        choices=['none', 'alert', 'crit','err','warning','info','debug'],
+                        default = 'none'
+                        )
+    
+    parser.add_argument('-of','--outputfile',
+                        help='Filename for output', #Customize messages as needed
+                        action='store',
+                        type = str,
+                        required=False,
+                        dest='outputfile',
+                        default = None
+                        )
+    
+    parser.add_argument('-if','--inputfile',
+                        help='Filename for input', #Customize messages as needed
+                        action='store',
+                        type = str,
+                        required=False,
+                        dest='inputfile',
+                        default = None
+                        )
+    
+    parser.add_argument('-od','--outputdir',
+                        help='Directory for output', #Customize messages as needed
+                        action='store',
+                        type = str,
+                        required=False,
+                        dest='outputdir',
+                        default = None
+                        )
+    
+    parser.add_argument('-if','--inputfile',
+                        help='Directory for Input', #Customize messages as needed
+                        action='store',
+                        type = str,
+                        required=False,
+                        dest='inputdir',
+                        default = None
+                        )    
+    
+    parser.add_argument('-f','--format',
+                        help='Directory for Input', #Customize messages as needed
+                        action='store',
+                        type = str,
+                        required=False,
+                        choices=['text', 'csv', 'xls','json'],
+                        dest='outputformat',
+                        default = None
+                        ) 
+    
+    parser.add_argument('-dr','--dryrun',
+                        help='Display what would be done. Create no objects, Modify no objects, Delete no objects', #Customize messages as needed
+                        action='store_true',
+                        type = bool,
+                        required=False,
+                        dest='dryrun',
+                        default = False
+                        )       
+    parse_out = parser.parse_args()
+    return parse_out
